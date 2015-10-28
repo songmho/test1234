1b06e12a378b2fbd66f5b249862196ff0edd5e29
songmho <songmho@gmail.com>
2015-10-28 23:42:33 +0900

    타이머 추가, 랜덤 추가

--- app/src/main/java/com/songmho/myapplication/MainActivity.java
+++ app/src/main/java/com/songmho/myapplication/MainActivity.java
@@ -4,10 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import java.util.Random;
+import android.widget.TextView;
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,30 +14,79 @@ public class MainActivity extends AppCompatActivity {
     Button bit2;
     Button bit3;
     Button bit4;
+    TextView textView;
+
+    boolean [] isclicked={false,false,false,false};
+
+    int []color={0xff123abc,0xff321ABC,0XFF54AB12,0XFF126532};
+    int time=10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
-
-
-       int ran=(int) (Math.random()%8);
         bit1=(Button)findViewById(R.id.bit1);
         bit2=(Button)findViewById(R.id.bit2);
         bit3=(Button)findViewById(R.id.bit3);
         bit4=(Button)findViewById(R.id.bit4);
+        textView=(TextView)findViewById(R.id.textView);
+
+
+        bit1.setBackgroundColor(color[(int)((Math.random()*10)%4)]);
+        bit2.setBackgroundColor(color[(int)((Math.random()*10)%4)]);
+        bit3.setBackgroundColor(color[(int)((Math.random()*10)%4)]);
+        bit4.setBackgroundColor(color[(int)((Math.random()*10)%4)]);
+
+        Timer timer=new Timer();
+
+        Timer timer1=new Timer();
+        TimerTask timerTask=new MyTimerTask();
+
+        timer.schedule(timerTask,3000,1000);
+
         bit1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "메롱", Toast.LENGTH_SHORT).show();
-                bit1.setBackgroundColor(0x00ff0033);
+                if(!isclicked[0]) {
+                    bit1.setBackgroundColor(0xffff0033);
+                    isclicked[0]=true;
+                }
+                else{
+                    bit1.setBackgroundColor(0xffcccccc);
+                    isclicked[0]=false;
+                }
             }
         });
     }
 
-    Timer timer=new Timer();
+    private class MyTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            new Thread(){
+                @Override
+                public void run() {
+                    super.run();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+
+
+                            bit1.setBackgroundColor(0xffcccccc);
+                            bit2.setBackgroundColor(0xffcccccc);
+                            bit3.setBackgroundColor(0xffcccccc);
+                            bit4.setBackgroundColor(0xffcccccc);
+                            if(time==0)
+                                cancel();
+
+                            textView.setText(""+time);
+
+                            time--;
+                        }
+                    });
+                }
+            }.start();
+        }
+    }
 }
